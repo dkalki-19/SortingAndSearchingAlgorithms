931621aaefab9f33d9940734798f503cd64590af
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-21 11:03:01 +0530

    Sorting Algorithms

---
+++ src/sortingalgorithms/BubbleSort.java
@@ -0,0 +1,17 @@
+package sortingalgorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] marks = {50, 40, 70, 10, 90};
+        for (int i = 0; i < marks.length - 1; i++) {
+            for (int j = 0; j < marks.length - 1 - i; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                }
+            }
+        }
+        for (int m : marks) System.out.print(m + " ");
+    }
+}
